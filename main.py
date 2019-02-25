b46035e09d8841d653d8529ed5ba02f026680f1c
makeitokay <andrey.mu11@yandex.ru>
2019-02-25 23:08:16 +0500

    main.py разделен на run.py - файл, запускающий сервер, и app.py - инициализация приложения

--- main.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-from config import HOST, PORT
-
-app = Flask(__name__)
-app.run(port=PORT, host=HOST)
---
+++ run.py
@@ -0,0 +1,4 @@
+from app import app
+from config import PORT, HOST
+
+app.run(port=PORT, host=HOST)
