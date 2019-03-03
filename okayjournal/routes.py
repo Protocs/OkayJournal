3b73dc86aacbdacb52f26e665805f6cdb9401d6f
makeitokay <andrey.mu11@yandex.ru>
2019-03-03 22:08:57 +0500

    Прототип index.html

--- okayjournal/routes.py
+++ okayjournal/routes.py
@@ -7,7 +7,7 @@ from okayjournal.forms import LoginForm
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("base.html")
+    return render_template("index.html")
 
 
 @app.route('/login', methods=['GET', 'POST'])
